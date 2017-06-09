75746f6e064de26a32a0482cf1b7e93e2b708a28
Rick Wardenburg <slickrickstyle@gmail.com>
2017-06-09 05:02:11 -0500

    Cleanup

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
-export PATH=$(pwd)/arm-eabi-4.8/bin:$PATH
+export PATH=/usr/local/toolchain/UBERTC-arm-eabi-4.8/bin:$PATH
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=/usr/local/toolchain/UBERTC-arm-eabi-4.8/bin/arm-eabi-
 
 mkdir $(pwd)/out
 make -C $(pwd) O=$(pwd)/out msm8929_sec_defconfig VARIANT_DEFCONFIG=msm8929_sec_j7_spr_defconfig SELINUX_DEFCONFIG=selinux_defconfig
