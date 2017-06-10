599438991450cde9040f04a117cc72a00c09911e
Rick Wardenburg <slickrickstyle@gmail.com>
2017-06-10 00:00:49 -0500

    fix error

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -337,8 +337,8 @@ static int enter_state(suspend_state_t state)
 	printk(KERN_INFO "PM: Syncing filesystems ... ");
 	sys_sync();
 	printk("done.\n");
-#endif
-	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state]);
+
+	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state].label);
 	error = suspend_prepare(state);
 	if (error)
 		goto Unlock;
